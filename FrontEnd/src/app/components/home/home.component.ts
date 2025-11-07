2dfbed7db2945901d41a4ce6b34111cdf8fb0202
mostafawael4 <mostafawael556@gmail.com>
2025-11-07 02:55:47 +0200

    Refactor home component to load images from backend API with loading state; remove static image JSON and associated files; enhance styling for loading spinner.

--- FrontEnd/src/app/components/home/home.component.ts
+++ FrontEnd/src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import imageData from '../../../assets/images.json';
+import { GalleryService, GalleryImage } from '../../services/gallery.service';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +10,37 @@ import imageData from '../../../assets/images.json';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
-  images: string[] = [];
+  images: GalleryImage[] = [];
   loadedImages: Set<number> = new Set();
+  isLoading: boolean = true;
+
+  constructor(private galleryService: GalleryService) {}
 
   ngOnInit() {
-    // Load images from JSON file
-    this.images = imageData.gallery;
+    // Load random images from backend API
+    this.loadRandomImages();
+  }
+
+  loadRandomImages() {
+    this.isLoading = true;
+    this.galleryService.getRandomImages(6).subscribe({
+      next: (images) => {
+        this.images = images;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error loading random images:', error);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  getImageUrl(image: GalleryImage): string {
+    // If the URL is relative, prepend the backend URL
+    if (image.url.startsWith('/')) {
+      return `http://localhost:4000${image.url}`;
+    }
+    return image.url;
   }
 
   onImageLoad(index: number) {
