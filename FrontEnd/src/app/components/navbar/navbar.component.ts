55fb9d0cb8af1f5836d1cd71016e8293bb214eb2
mostafawael4 <mostafawael556@gmail.com>
2025-11-16 01:11:42 +0200

    Enhance navbar component by adding routerLinkActive directives for active link styling and updating SCSS to manage hover and active states. This improves user experience by clearly indicating the current page in the navigation menu.

--- FrontEnd/src/app/components/navbar/navbar.component.ts
+++ FrontEnd/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,12 @@
 import { Component, HostListener, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, Router } from '@angular/router';
+import { RouterLink, Router, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
